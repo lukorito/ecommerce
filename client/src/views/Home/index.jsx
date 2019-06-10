de01c37061ab89e7a84d5eab716a51c5b4ef1d55
Kelvin Lukorito <kelvin.lukorito@andela.com>
2019-06-10 19:38:54 +0300

    Add front-end

---
+++ client/src/views/Home/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../style.scss';
+
+class Home extends Component{
+  render(){
+    return (
+      <div className="container">
+      This is the home page
+      </div>
+    );
+  }
+}
+
+export default Home;
